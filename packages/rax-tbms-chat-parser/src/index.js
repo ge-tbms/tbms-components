3cbba26730402dac39d15dae1b56f6b0efe34daa
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-26 20:54:26 +0800

    feat: component build

---
+++ packages/rax-tbms-chat-parser/src/index.js
@@ -0,0 +1,5 @@
+import {Component, createElement, PropTypes, render} from 'rax';
+import {isWeex, isWeb} from 'universal-env';
+import View from 'rax-view';
+import Text from 'rax-text';
+import Image from 'rax-image';
---
+++ readme.md
@@ -0,0 +1,10 @@
+# components
+## 项目描述
+tbms, components,
+
+## License
+
+MIT © [matrix]()
+
+
+[npm-packages]: https://www.npmjs.com/~dkypooh
